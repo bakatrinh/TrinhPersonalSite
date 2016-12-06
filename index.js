296c44217d37965ce6015e4cc24a20fa8c8b0924
Trinh Nguyen <trinh.d.nguyen@sjsu.edu>
2016-12-05 17:25:04 -0800

    new redirects

--- index.js
+++ index.js
@@ -12,10 +12,21 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/bug', function(req,res){
+	//console.log(__dirname);
+    filePath = __dirname + '/public/buggerwars/index.html';
+    res.sendFile(filePath);
+});
+
+app.get('/bugger', function(req,res){
+    filePath = __dirname + '/public/buggerwars/index.html';
+    res.sendFile(filePath);
+});
+
 // app.get('/', function(request, response) {
 //   response.render('public/views');
 // });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
